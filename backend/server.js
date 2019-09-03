0f91d313a58745d71e89db335245c54d166b7d36
Gian <giangravina@gmail.com>
2019-09-03 18:05:58 -0400

    version 1.0.0

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
+
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+	//set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+
+}
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -23,6 +36,7 @@ const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
