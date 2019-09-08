459116c186d2f426e987760a95d3d0b821529437
Gian <giangravina@gmail.com>
2019-09-08 18:56:09 -0400

    initial commit

---
+++ client/build/precache-manifest.3eb16778db8292c5ce0ffc0a4b0d1520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ac3bccfc18df62b63c7a32b1d2e355",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df1ba125b08013e80bb0",
+    "url": "/static/css/2.dea020aa.chunk.css"
+  },
+  {
+    "revision": "df1ba125b08013e80bb0",
+    "url": "/static/js/2.775c3c33.chunk.js"
+  },
+  {
+    "revision": "934b0f4d16d527d655db",
+    "url": "/static/js/main.7761cb03.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
